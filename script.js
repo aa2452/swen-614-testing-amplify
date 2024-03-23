ddcc6b0038a77d5b19681bd32d61198ced59689a
aa2452 <61297488+aa2452@users.noreply.github.com>
2024-03-23 03:04:12 -0400

    Added the Files Group
    Functioning File Group

---
+++ script.js
@@ -0,0 +1,58 @@
+document.write('<script src="./aws-exports.js"></script>');
+
+
+AWS.config.update({
+        accessKeyId: ROOT_KEY,
+        secretAccessKey: SECRET_KEY,
+        region: 'us-east-2'
+});
+
+
+
+const videoElement = document.getElementById('videoElement');
+const canvasElement = document.getElementById('canvasElement');
+const uploadButton = document.querySelector('button');
+
+let mediaStream;
+
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function(stream) {
+        videoElement.srcObject = stream;
+        mediaStream = stream;
+    })
+    .catch(function(error) {
+        console.error('Error accessing webcam:', error);
+    });
+
+function captureAndUpload() {
+    const context = canvasElement.getContext('2d');
+    context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+
+    canvasElement.toBlob(function(blob) {
+        const bucketName = 'aa2452-swen-614-team-bucket';
+        const region = 'us-east-2';
+        const key =  Date.now() + '.png';
+
+        const s3 = new AWS.S3({
+            apiVersion: '2006-03-01',
+            region: region
+        });
+
+        const params = {
+            Bucket: bucketName,
+            Key: key,
+            Body: blob,
+            ACL: '',
+            ContentType: 'image/png'
+        };
+
+        s3.upload(params, function(err, data) {
+            if (err) {
+                console.error('Error uploading image:', err);
+            } else {
+                console.log('Image uploaded successfully:', data.Location);
+                alert('Image uploaded successfully!');
+            }
+        });
+    }, 'image/png');
+}
