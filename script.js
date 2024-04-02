34c749f6115e45823d67d37ec86fa271af6e455b
aa2452 <61297488+aa2452@users.noreply.github.com>
2024-04-01 21:51:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function captureAndUpload() {
     context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
 
     canvasElement.toBlob(function(blob) {
-        const bucketName = 'swen_614_source_bucket';
+        const bucketName = 'aa2452-swen-614-team-bucket';
         const region = 'us-east-2';
         const key =  Date.now() + '.png';
 
